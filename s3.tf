4759e935c4ee9c7267704bd5308ee6a71f4bc6a5
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-04 23:51:27 -0400

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "my-log-s3-bucket333"
+  tags   = local.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

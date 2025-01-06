8a8f65368772a4405c85cd107c8b763e18789734
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-06 10:07:06 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 # VPC Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"
+  version = "5.17.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -10,11 +10,17 @@ module "vpc" {
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
   intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+# database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 56)]
+
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+
+  # create_database_subnet_group       = true
+  # create_database_subnet_route_table = true
+
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
@@ -26,6 +32,10 @@ module "vpc" {
     "karpenter.sh/discovery"         = local.name
   }
 
+  # database_subnet_tags = {
+  #    "subnet_type" = "database"
+  # }
+ 
   tags = local.tags
 }
 

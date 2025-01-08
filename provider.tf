c8d657c8225031cefcb57b9bbb495cff6c67f2df
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-07 21:02:10 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.9.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10.0"
+    }
   }
   required_version = ">= 1.0"
 }
@@ -22,11 +26,21 @@ provider "aws" {
   region = local.region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"

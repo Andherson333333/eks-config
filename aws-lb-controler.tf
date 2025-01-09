31f29c6abbc2985e6f2fdc169b541fa5c8ccf460
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-09 12:00:29 -0400

    Update aws-lb-controler.tf

--- aws-lb-controler.tf
+++ aws-lb-controler.tf
@@ -1,7 +1,7 @@
 # AWS Module loadbalancer controler
 module "aws_lb_controller_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
-  version = "1.4.0"
+  version = "1.9.0"
 
   name = "aws-lbc"
   attach_aws_lb_controller_policy = true

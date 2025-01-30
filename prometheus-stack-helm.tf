4cf086213e7ff563a91bb3c518428e2c3595d340
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-30 11:31:50 -0400

    Update prometheus-stack-helm.tf

--- prometheus-stack-helm.tf
+++ prometheus-stack-helm.tf
@@ -1,56 +1,58 @@
+# Helm release for Prometheus Stack installation
 resource "helm_release" "prometheus_stack" {
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  version          = "68.2.1"
-  namespace        = "monitoring"
-  create_namespace = true
+ name             = "prometheus"
+ repository       = "https://prometheus-community.github.io/helm-charts"  
+ chart            = "kube-prometheus-stack"
+ version          = "68.2.1"
+ namespace        = "monitoring"
+ create_namespace = true
 
-  # Configuraciones básicas de ServiceMonitor y PodMonitor
-  set {
-    name  = "serviceMonitor.metadata.labels.release"
-    value = "prometheus"
-  }
+ # Basic ServiceMonitor and PodMonitor configurations
+ set {
+   name  = "serviceMonitor.metadata.labels.release"
+   value = "prometheus"
+ }
+ set {
+   name  = "podMonitor.metadata.labels.release" 
+   value = "prometheus"
+ }
 
-  set {
-    name  = "podMonitor.metadata.labels.release"
-    value = "prometheus"
-  }
-  set {
-    name  = "prometheus.prometheusSpec.retention"
-    value = "15d"
-  }
+ # Prometheus storage configuration
+ set {
+   name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.storageClassName"
+   value = "gp3-default"
+ }
+ set {
+   name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage"
+   value = "10Gi"
+ }
+ set {
+   name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.accessModes[0]"
+   value = "ReadWriteOnce"
+ }
 
-  # Configuración de almacenamiento para Prometheus
-  set {
-    name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.storageClassName"
-    value = "gp3-default"
-  }
-  set {
-    name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage"
-    value = "10Gi"
-  }
-  set {
-    name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.accessModes[0]"
-    value = "ReadWriteOnce"
-  }
+ # Grafana storage configuration
+ set {
+   name  = "grafana.persistence.enabled"
+   value = "true"
+ }
+ set {
+   name  = "grafana.persistence.storageClassName"
+   value = "gp3-default"
+ }
+ set {
+   name  = "grafana.persistence.size"
+   value = "10Gi"
+ }
 
-  # Configuración de almacenamiento para Grafana
-  set {
-    name  = "grafana.persistence.enabled"
-    value = "true"
-  }
-  set {
-    name  = "grafana.persistence.storageClassName"
-    value = "gp3-default"
-  }
-  set {
-    name  = "grafana.persistence.size"
-    value = "10Gi"
-  }
+ # Retention configuration
+ set {
+   name  = "prometheus.prometheusSpec.retention"
+   value = "15d"
+ }
 
-  depends_on = [
-    helm_release.aws_lbc,
-    helm_release.external_nginx
-  ]
+ depends_on = [
+   helm_release.aws_lbc,
+   helm_release.external_nginx
+ ]
 }

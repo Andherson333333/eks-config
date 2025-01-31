d974b4528a9d0fc39832f067e80a5b2328642969
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-31 08:46:43 -0400

    Update argocd-helm.tf

--- argocd-helm.tf
+++ argocd-helm.tf
@@ -1,42 +1,39 @@
+# Base ArgoCD Installation Block
+# Defines the main configuration for ArgoCD deployment
 resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  version          = "7.7.13"
-  create_namespace = true
+ name             = "argocd"
+ repository       = "https://argoproj.github.io/argo-helm"
+ chart            = "argo-cd" 
+ namespace        = "argocd"
+ version          = "7.7.13"
+ create_namespace = true
 
-  # Redis configuration
-  set {
-    name  = "redis.enabled"
-    value = "true"
-  }
-  set {
-    name  = "redis.persistence.enabled"
-    value = "true"
-  }
-  set {
-    name  = "redis.persistence.storageClass"
-    value = "gp3-default"
-  }
-  set {
-    name  = "redis.persistence.size"
-    value = "10Gi"
-  }
-  set {
-    name  = "redis.persistence.accessMode"
-    value = "ReadWriteOnce"
-  }
+ # Storage Configuration Block
+ # Sets up persistent storage settings for Redis
+ set {
+   name  = "redis.enabled"
+   value = "true"
+ }
+ set {
+   name  = "redis.persistence.enabled" 
+   value = "true"
+ }
+ set {
+   name  = "redis.persistence.storageClass"
+   value = "gp3-default"
+ }
 
-  # Server configuration
-  set {
-    name  = "configs.params.server\\.insecure"
-    value = "true"
-  }
+ # ArgoCD Server Configuration Block
+ # Configures core ArgoCD server settings
+ set {
+   name  = "configs.params.server\\.insecure"
+   value = "true"
+ }
 
-  depends_on = [
-    helm_release.nginx_internal,
-    helm_release.external_nginx
-  ]
+ # Dependencies Block
+ # Defines required resources that must exist first
+ depends_on = [
+   helm_release.nginx_internal,
+   helm_release.external_nginx
+ ]
 }
-

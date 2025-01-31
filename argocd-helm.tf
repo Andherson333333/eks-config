eb45904b57fab15f64ee0edf237dcdc7dc2afca8
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-31 08:41:31 -0400

    Update argocd-helm.tf

--- argocd-helm.tf
+++ argocd-helm.tf
@@ -6,46 +6,37 @@ resource "helm_release" "argocd" {
   version          = "7.7.13"
   create_namespace = true
 
+  # Redis configuration
   set {
     name  = "redis.enabled"
     value = "true"
   }
-
   set {
     name  = "redis.persistence.enabled"
     value = "true"
   }
-
   set {
-    name  = "redis.persistence.existingClaim"
-    value = "argocd-pvc"
+    name  = "redis.persistence.storageClass"
+    value = "gp3-default"
+  }
+  set {
+    name  = "redis.persistence.size"
+    value = "10Gi"
+  }
+  set {
+    name  = "redis.persistence.accessMode"
+    value = "ReadWriteOnce"
   }
 
+  # Server configuration
   set {
     name  = "configs.params.server\\.insecure"
     value = "true"
   }
 
-  # Agregar estas configuraciones para el basePath
-
-  set {
-    name  = "configs.params.server\\.basehref"
-    value = "/argocd"
-  }
-
-  set {
-    name  = "server.basePath"
-    value = "/argocd"
-  }
-
-  set {
-    name  = "server.baseUrl"
-    value = "/argocd"
-  }
-
   depends_on = [
-    kubectl_manifest.argocd_pvc,
     helm_release.nginx_internal,
     helm_release.external_nginx
   ]
 }
+

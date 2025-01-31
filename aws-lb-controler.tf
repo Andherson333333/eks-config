f9509621c4e12f8b9728a725e22239ae6a6b2c66
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-01-31 08:49:49 -0400

    Update aws-lb-controler.tf

--- aws-lb-controler.tf
+++ aws-lb-controler.tf
@@ -1,25 +1,25 @@
-# AWS Module loadbalancer controler
+# AWS Load Balancer Controller Module
 module "aws_lb_controller_pod_identity" {
-  source  = "terraform-aws-modules/eks-pod-identity/aws"
-  version = "1.9.0"
+ source  = "terraform-aws-modules/eks-pod-identity/aws"
+ version = "1.9.0"
 
-  name = "aws-lbc"
-  attach_aws_lb_controller_policy = true
+ # Basic Configuration Block
+ name = "aws-lbc"
+ attach_aws_lb_controller_policy = true
 
+ # Default Identity Association Settings
+ association_defaults = {
+   namespace       = "kube-system"
+   service_account = "aws-load-balancer-controller" 
+ }
 
-  # Configuración por defecto para las asociaciones de identidad
-  association_defaults = {
-    namespace       = "kube-system"
-    service_account = "aws-load-balancer-controller"
-  }
+ # Cluster Association Block
+ associations = {
+   eks_one = {
+     cluster_name = module.eks.cluster_name
+   }
+ }
 
-  associations = {
-    eks_one = {
-      cluster_name = module.eks.cluster_name
-    }
-  }
-
-  tags = local.tags
-
-  depends_on = [module.eks]
+ tags = local.tags
+ depends_on = [module.eks]
 }
